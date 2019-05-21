c8767037090c1be8343fb4c40414fcb3fd372eb0
HATATANI Shinta <gamaguchi@gmail.com>
2019-05-21 13:58:40 +0900

    Implement delegate chain

--- main.go
+++ main.go
@@ -16,18 +16,21 @@ import (
 	"google.golang.org/api/iamcredentials/v1"
 )
 
-type scopesType []string
+type stringsType []string
 
-func (ss *scopesType) String() string {
+var defaultScope = []string{
+"https://www.googleapis.com/auth/cloud-platform",
+"https://www.googleapis.com/auth/userinfo.email",
+}
+
+func (ss *stringsType) String() string {
 	return fmt.Sprintf("%v", *ss)
 }
 
-func (ss *scopesType) Set(v string) error {
+const authPrefix = "https://www.googleapis.com/auth/"
+
+func (ss *stringsType) Set(v string) error {
 	for _, scope := range strings.Split(v, ",") {
-		const authPrefix = "https://www.googleapis.com/auth/"
-		if !strings.HasPrefix(scope, authPrefix) {
-			scope = authPrefix + scope
-		}
 		*ss = append(*ss, scope)
 	}
 	return nil
@@ -48,50 +51,67 @@ func GcloudIdToken() (string, error) {
 
 func main() {
 	var err error
-	var scopes scopesType
-	flag.Var(&scopes, "scopes", "Scopes")
+	var rawScopes stringsType
+	flag.Var(&rawScopes, "scopes", "Scopes")
+	var impersonateServiceAccount stringsType
+	flag.Var(&impersonateServiceAccount, "impersonate-service-account", "Delegates")
 	var printToken = flag.Bool("print-token", false, "Print token")
 	var accessToken = flag.Bool("access-token", false, "Use access token")
 	var audience = flag.String("audience", "", "Audience")
 	var idToken = flag.Bool("id-token", false, "Use ID token")
-	var serviceAccount = flag.String("service-account", "", "Service Account")
 	var tokenInfo = flag.Bool("token-info", false, "Print token info")
 	flag.Parse()
 
+	var serviceAccount string
+	var delegateChain []string
+	if len(impersonateServiceAccount) > 0 {
+		serviceAccount = impersonateServiceAccount[len(impersonateServiceAccount)-1]
+		delegateChain = impersonateServiceAccount[:len(impersonateServiceAccount)-1]
+	}
 	ctx := context.Background()
 
 	var tokenString string
 	switch {
 	case *idToken && *accessToken:
 		log.Fatalln("--id-token and --access-token are exclusive")
+	case *idToken && serviceAccount != "" && *audience == "":
+		log.Fatalln("--audience is required when --id-token is used")
 	case !*idToken && !*accessToken:
 		log.Fatalln("--id-token or --access-token are required")
-	case *idToken && len(scopes) != 0:
+	case *idToken && len(rawScopes) != 0:
 		log.Fatalln("--id-token and --scopes are exclusive")
 	case *accessToken && *audience != "":
 		log.Fatalln("--access-token and --audience are exclusive")
 	}
 
+	var scopes []string
+	for _, s := range rawScopes {
+		if !strings.HasPrefix(s, authPrefix) {
+			s = authPrefix + s
+		}
+		scopes = append(scopes, s)
+	}
+
+	if len(scopes) == 0 {
+		scopes = defaultScope
+	}
+
 	switch {
-	case *idToken && *serviceAccount == "":
+	case *idToken && serviceAccount == "":
 		log.Println("--service-account is missing. Use experimental gcloud ID token.")
 		tokenString, err = GcloudIdToken()
-	case *idToken && *serviceAccount != "":
-		tokenString, err = ImpersonateIdToken(ctx, *serviceAccount, audience)
-	case *accessToken && *serviceAccount == "":
+	case *idToken && serviceAccount != "":
+		tokenString, err = ImpersonateIdToken(ctx, serviceAccount, delegateChain, audience)
+	case *accessToken && serviceAccount == "":
 		tokenString, err = DefaultAccessToken(ctx)
-	case *accessToken && *serviceAccount != "":
-		tokenString, err = ImpersonateAccessToken(ctx, *serviceAccount, scopes)
+	case *accessToken && serviceAccount != "":
+		tokenString, err = ImpersonateAccessToken(ctx, serviceAccount, delegateChain, scopes)
 	default:
 		log.Fatalln("unknown branch")
 	}
 
 	if err != nil {
 		log.Fatalln(err)
-	}
-
-	if len(scopes) == 0 {
-		scopes = []string{"https://www.googleapis.com/auth/cloud-platform"}
 	}
 
 	if *printToken {
@@ -126,7 +146,7 @@ func main() {
 }
 
 func DefaultAccessToken(ctx context.Context) (string, error) {
-	tokenSource, err := google.DefaultTokenSource(ctx)
+	tokenSource, err := google.DefaultTokenSource(ctx, defaultScope...)
 	if err != nil {
 		return "", err
 	}
@@ -141,7 +161,15 @@ func toName(serviceAccount string) string {
 	return "projects/-/serviceAccounts/" + serviceAccount
 }
 
-func ImpersonateIdToken(ctx context.Context, serviceAccount string, audience *string) (string, error) {
+func toNameSlice(serviceAccounts []string) []string {
+	var slice []string
+	for _, s := range serviceAccounts {
+		slice = append(slice, toName(s))
+	}
+	return slice
+}
+
+func ImpersonateIdToken(ctx context.Context, serviceAccount string, delegateChain []string, audience *string) (string, error) {
 	service, err := iamcredentials.NewService(ctx)
 	if err != nil {
 		return "", err
@@ -151,6 +179,7 @@ func ImpersonateIdToken(ctx context.Context, serviceAccount string, audience *st
 	response, err := projectsService.ServiceAccounts.GenerateIdToken(toName(serviceAccount),
 		&iamcredentials.GenerateIdTokenRequest{
 			Audience:     *audience,
+			Delegates: toNameSlice(delegateChain),
 			IncludeEmail: true,
 		}).Do()
 	if err != nil {
@@ -159,7 +188,7 @@ func ImpersonateIdToken(ctx context.Context, serviceAccount string, audience *st
 	return response.Token, nil
 }
 
-func ImpersonateAccessToken(ctx context.Context, serviceAccount string, scopes []string) (string, error) {
+func ImpersonateAccessToken(ctx context.Context, serviceAccount string, delegateChain []string, scopes []string) (string, error) {
 	service, err := iamcredentials.NewService(ctx)
 	if err != nil {
 		return "", err
@@ -169,6 +198,7 @@ func ImpersonateAccessToken(ctx context.Context, serviceAccount string, scopes [
 	response, err := projectsService.ServiceAccounts.GenerateAccessToken(toName(serviceAccount),
 		&iamcredentials.GenerateAccessTokenRequest{
 			Scope: scopes,
+			Delegates: toNameSlice(delegateChain),
 		}).Do()
 	if err != nil {
 		return "", err
