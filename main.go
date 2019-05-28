59c4aa9126b8146a2663f1bad3bcebc2d562046e
HATATANI Shinta <gamaguchi@gmail.com>
2019-05-29 00:43:23 +0900

    Fix comment

--- main.go
+++ main.go
@@ -67,7 +67,7 @@ func main() {
 
 	ctx := context.Background()
 
-	// --gcloudFlag-account implies --gcloudFlag
+	// --gcloud-account implies --gcloud
 	if *gcloudAccount != "" {
 		*gcloudFlag = true
 	}
